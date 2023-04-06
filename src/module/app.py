fb29e17f37586894ecb7feb7966479053b6c366f
DidulSeneviratne <dulsaradidul@gmail.com>
2023-04-06 10:25:35 +0530

    no message

---
+++ src/module/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, Response
+from model import model
+
+
+app = Flask(__name__)
+
+@app.route('/video_feed')
+def index():
+    return Response(model(), mimetype='multipart/x-mixed-replace; boundary=frame' )
+
+if __name__ == '__main__':
+    app.run(debug=False, host=' 0.0.0.0 ')
